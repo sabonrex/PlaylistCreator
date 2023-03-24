b728c38674a05c5bb798779308ea2c1168e8a993
sabonrex <sabona.a.rexwinkel@gmail.com>
2023-03-24 19:18:27 +0000

    sabonrexadd
    addloginandsuch

--- src/api/routes.py
+++ src/api/routes.py
@@ -5,6 +5,14 @@ from flask import Flask, request, jsonify, url_for, Blueprint
 from api.models import db, User
 from api.utils import generate_sitemap, APIException
 
+
+
+from flask_jwt_extended import create_access_token
+from flask_jwt_extended import get_jwt_identity
+from flask_jwt_extended import jwt_required
+from flask_jwt_extended import JWTManager
+
+
 api = Blueprint('api', __name__)
 
 
@@ -16,3 +24,51 @@ def handle_hello():
     }
 
     return jsonify(response_body), 200
+
+#add sign up
+
+@api.route("/signup", methods=["POST"])
+def add_user():
+    user = User()
+
+    request_user = request.json
+
+    user.email = request.json.get("email", None)
+    user.password = request_user["password"]
+
+    # Here we should add password encrypton
+
+    user.is_active = True
+
+    db.session.add(user)
+    db.session.commit()
+
+    response = {
+        "msg": "User successfully created"
+    }
+
+    return jsonify(response), 201
+
+@api.route("/login", methods=["POST"])
+def login():
+    email = request.json.get("email", None)
+    password = request.json.get("password", None)
+
+    user = User.query.filter_by(email=email).first()
+
+    if password != user.password:
+        return jsonify({"msg": "Bad email or password"}), 401
+
+    access_token = create_access_token(identity=email)
+    return jsonify(access_token=access_token)
+
+#Add to check user
+
+@api.route("/check", methods=["GET"])
+@jwt_required()
+def protected():
+    # Access the identity of the current user with get_jwt_identity
+    current_email = get_jwt_identity()
+    user = User.query.filter_by(email=current_email).first()
+
+    return jsonify(user.serialize()),200
--- src/app.py
+++ src/app.py
@@ -12,12 +12,18 @@ from api.routes import api
 from api.admin import setup_admin
 from api.commands import setup_commands
 
+from flask_jwt_extended import JWTManager
+
 #from models import Person
 
 ENV = os.getenv("FLASK_ENV")
 static_file_dir = os.path.join(os.path.dirname(os.path.realpath(__file__)), '../public/')
 app = Flask(__name__)
 app.url_map.strict_slashes = False
+
+# Setup the Flask-JWT-Extended extension
+app.config["JWT_SECRET_KEY"] = "super-secret"  # Change this!
+jwt = JWTManager(app)
 
 # database condiguration
 db_url = os.getenv("DATABASE_URL")
@@ -30,56 +36,7 @@ app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 MIGRATE = Migrate(app, db, compare_type = True)
 db.init_app(app)
 
-#add sign up
 
-@app.route("/sign", methods=["POST"])
-def add_user():
-    user = User()
-
-    request_user = request.json
-
-    user.email = request.json.get("email", None)
-    user.password = request_user["password"]
-
-    # Here we should add password encrypton
-
-    user.is_active = True
-
-    db.session.add(user)
-    db.session.commit()
-
-    response = {
-        "msg": "User successfully created"
-    }
-
-    return jsonify(response), 201
-
-
-#Add Log In
-
-@app.route("/login", methods=["POST"])
-def login():
-    email = request.json.get("email", None)
-    password = request.json.get("password", None)
-
-    user = User.query.filter_by(email=email).first()
-
-    if password != user.password:
-        return jsonify({"msg": "Bad email or password"}), 401
-
-    access_token = create_access_token(identity=email)
-    return jsonify(access_token=access_token)
-
-#Add to check user
-
-@app.route("/check", methods=["GET"])
-@jwt_required()
-def protected():
-    # Access the identity of the current user with get_jwt_identity
-    current_email = get_jwt_identity()
-    user = User.query.filter_by(email=current_email).first()
-
-    return jsonify(user.serialize()), 200
 
 # Allow CORS requests to this API
 CORS(app)
