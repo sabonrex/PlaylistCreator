509a940605838bc793bf81fee94c5a7bc6403ede
José Inácio Castelo Branco <zeinaciocb@gmail.com>
2023-03-25 01:04:13 +0000

    feat: main view added to the layout. Boilerplate home view can go away

--- src/front/js/layout.js
+++ src/front/js/layout.js
@@ -3,6 +3,7 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import ScrollToTop from "./component/scrollToTop";
 
 import { Home } from "./pages/home";
+import { MainView } from "./pages/mainView.jsx";
 import { Demo } from "./pages/demo";
 import { Single } from "./pages/single";
 import { Login } from "./pages/login.jsx";
@@ -24,7 +25,7 @@ const Layout = () => {
                 <ScrollToTop>
                     <Navbar />
                     <Routes>
-                        <Route element={<Home />} path="/" />
+                        <Route element={<MainView />} path="/" />
                         <Route element={<Demo />} path="/demo" />
                         <Route element={<Signup />} path="/signup" />
                         <Route element={<Login />} path="/login" />
