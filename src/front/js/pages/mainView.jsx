ba42e9e0c71efec4d51955171539149f35c91d53
José Inácio Castelo Branco <zeinaciocb@gmail.com>
2023-03-26 18:41:34 +0000

    feat: mainView styles added

--- src/front/js/pages/mainView.jsx
+++ src/front/js/pages/mainView.jsx
@@ -1,15 +1,20 @@
-import React from "react";
+import React, { useState } from "react";
 
 import "../../styles/index.css";
 
 export const MainView = () => {
 
+  const [playlist, setPlaylist] = useState(null)
+
+  const playlistDiv = null;
+
   return (
     <section className="h-100 w-100 py-5" style={{"backgroundColor": "#1D2343"}}>
-        <div className="container text-center">
-            <h1 className="jumbo-text">Find Your <span style={{"color": "#BAFF4F"}}>Music</span></h1>
-            <button>Discover</button>
+        <div className="container text-center py-5 my-5">
+            <h1 className="jumbo-text my-5">Find Your <span style={{"color": "#BAFF4F", "fontWeight": "bold"}}>Music</span></h1>
+            <button className="discover-button my-5">Discover your Playlist</button>
         </div>
+        {playlistDiv}
     </section>
   );
 };
