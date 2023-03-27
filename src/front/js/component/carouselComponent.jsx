5b75c57239de84c97c96f74e032e27df44296f31
José Inácio Castelo Branco <zeinaciocb@gmail.com>
2023-03-27 18:30:36 +0000

    feat: align card items inside the carousel

--- src/front/js/component/carouselComponent.jsx
+++ src/front/js/component/carouselComponent.jsx
@@ -19,20 +19,20 @@ export const CarouselComponent = ({ tracks }) => {
             <Carousel controls={slides.length > 1} indicators={slides.length > 1} interval={3000} >
                 {slides.map((slide, index) => (
                 <Carousel.Item key={index}>
-                    <div className="row d-flex justify-content-center align-items-top" style={{ height: "100%", backgroundColor: "#1D2343" }} >
+                    <div className="row d-flex justify-content-center align-items-top pb-5" style={{ height: "100%", backgroundColor: "#1D2343" }} >
                         {slide.map((track) => (
                         <div key={track.id} className="col-sm-2">
-                            <div className="card shadow" style={{ backgroundColor: "#DC6B5E", borderRadius: "10px" }} >
+                            <div className="card shadow" style={{ backgroundColor: "#DC6B5E", borderRadius: "10px"}} >
                                 <img
                                     className="card-img-top"
                                     src={track.url}
                                     alt={track.alt}
-                                    style={{ borderRadius: "10px 10px 0 0" }}
+                                    style={{ borderRadius: "10px"}}
                                 />
-                                <div className="card-body text-light text-center">
-                                    <h5 className="card-title fs-5">{track.title}</h5>
-                                    <p className="card-text">{track.artist}</p>
-                                </div>
+                            </div>
+                            <div className="card-body text-light text-center">
+                                <h5 className="card-title fs-5">{track.title}</h5>
+                                <p className="card-text">{track.artist}</p>
                             </div>
                         </div>
                         ))}
