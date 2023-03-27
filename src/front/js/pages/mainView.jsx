aec73c83772e5ed2e5936274baf41b34f4c98ff1
romanconstantin1 <romanconstantin1@gmail.com>
2023-03-27 13:32:46 +0000

    feat: updated test data with dummy spotify data, mapping functions adjusted to go through new data

--- src/front/js/pages/mainView.jsx
+++ src/front/js/pages/mainView.jsx
@@ -1,12 +1,12 @@
 import React, { useState } from "react";
 
+import { Playlists } from "../component/playlistSelect";
+
 import "../../styles/index.css";
 
 export const MainView = () => {
 
   const [playlist, setPlaylist] = useState(null)
-
-  const playlistDiv = null;
 
   return (
     <section className="h-100 w-100 py-5" style={{"backgroundColor": "#1D2343"}}>
@@ -14,7 +14,7 @@ export const MainView = () => {
             <h1 className="jumbo-text my-5">Find Your <span style={{"color": "#BAFF4F", "fontWeight": "bold"}}>Music</span></h1>
             <button className="discover-button my-5">Discover your Playlist</button>
         </div>
-        {playlistDiv}
+        <Playlists />
     </section>
   );
 };
