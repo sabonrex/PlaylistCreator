9777bdc40a6d4e745afa1d2d2013792603c663d6
José Inácio Castelo Branco <zeinaciocb@gmail.com>
2023-03-28 17:38:00 +0000

    refactor: playlistSelect to use props

--- src/front/js/pages/home.js
+++ src/front/js/pages/home.js
@@ -1,6 +1,6 @@
 import React, { useContext } from "react";
 import { Context } from "../store/appContext";
-import { Playlists } from "../component/playlistSelect";
+import { Playlists } from "../component/playlistSelect.jsx";
 import { Link } from "react-router-dom";
 import rigoImageUrl from "../../img/rigo-baby.jpg";
 import "../../styles/home.css";
--- src/front/js/pages/mainView.jsx
+++ src/front/js/pages/mainView.jsx
@@ -1,20 +1,32 @@
 import React, { useState } from "react";
 
-import { Playlists } from "../component/playlistSelect";
+import { Playlists } from "../component/playlistSelect.jsx";
+import { playlistData } from "../component/testDataPlaylist";
 
 import "../../styles/index.css";
 
 export const MainView = () => {
+  const [playlist, setPlaylist] = useState(null);
 
-  const [playlist, setPlaylist] = useState(null)
+  const fetchPlaylist = () => {
+    console.log("vai po caralho");
+  };
 
   return (
-    <section className="h-100 w-100 py-5" style={{"backgroundColor": "#1D2343"}}>
-        <div className="container text-center py-5 my-5">
-            <h1 className="jumbo-text my-5">Find Your <span style={{"color": "#BAFF4F", "fontWeight": "bold"}}>Music</span></h1>
-            <button className="discover-button my-5">Discover your Playlist</button>
-        </div>
-        <Playlists />
+    <section
+      className="h-100 w-100 py-5"
+      style={{ backgroundColor: "#1D2343" }}
+    >
+      <div className="container text-center py-5 my-5">
+        <h1 className="jumbo-text my-5">
+          Find Your{" "}
+          <span style={{ color: "#BAFF4F", fontWeight: "bold" }}>Music</span>
+        </h1>
+        <button className="discover-button my-5" onClick={fetchPlaylist}>
+          Discover your Playlist
+        </button>
+      </div>
+      <Playlists playlist={playlistData} />
     </section>
   );
 };
