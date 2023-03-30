01c30122d6d556ce941594693a3198bc6e287e96
sabonrex <sabona.a.rexwinkel@gmail.com>
2023-03-30 01:07:22 +0000

    adding form
    finihsing touches

--- src/front/js/pages/loginRender.jsx
+++ src/front/js/pages/loginRender.jsx
@@ -6,22 +6,19 @@ import "../../styles/index.css";
 export const LoginRender = ({handleSubmit, handleChange}) => {
 
   return (
-    <section className="h-100 gradient-form" style={{"backgroundColor": "#eee"}}>
+    <section className="h-100 gradient-form" >
         <div className="container py-3 h-100">
             <div className="row d-flex justify-content-center align-items-center h-100">
                 <div className="col-xl-8">
-                    <div className="card rounded-3 text-black">
+                    <div>
                         <div className="row g-0">
                             <div className="col-lg-10 mx-auto">
-                                <div className="card-body p-md-5 mx-md-4">
-
+                                <div>
                                     <div className="text-center">
-                                        <img src="https://mdbcdn.b-cdn.net/img/Photos/new-templates/bootstrap-login-form/lotus.webp"
-                                            style={{"width": "185px"}} alt="logo"/>
-                                        <h4 className="mt-1 mb-5 pb-1">The Lotus Team</h4>
+                                    <h1>Log In</h1>
                                     </div>
-
-                                    <form name="login" onSubmit={handleSubmit}>
+                                    <form  name="login" onSubmit={handleSubmit}>
+                                        
                                         <div className="form-outline mb-4">
                                             <label className="form-label" htmlFor="email">Username</label>
                                             <input 
@@ -31,6 +28,7 @@ export const LoginRender = ({handleSubmit, handleChange}) => {
                                                 placeholder="Insert your email"
                                                 onChange={handleChange} />
                                         </div>
+                                    
 
                                         <div className="form-outline mb-4">
                                             <label className="form-label" htmlFor="password">Password</label>
