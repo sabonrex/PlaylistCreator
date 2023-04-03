6c95099707d561963f8d8833dc1f81b23083d1fb
José Inácio Castelo Branco <zeinaciocb@gmail.com>
2023-04-03 17:37:16 +0000

    fix(frontend): fixing the mainView after merging bugs

--- src/front/js/pages/mainView.jsx
+++ src/front/js/pages/mainView.jsx
@@ -28,22 +28,6 @@ export const MainView = () => {
         <button className="discover-button my-5" onClick={fetchPlaylist}>
           Discover your Playlist
         </button>
-        <SavePlaylistButton />
-        <Playlists />
-        <Favourites />
-      </div>
-
-    <div className="container text-center py-4 my-4">
-      <h1 className="jumbo-text my-5">
-        Find Your{" "}
-        <span style={{ color: "#BAFF4F", fontWeight: "bold" }}>Music</span>
-      </h1>
-      <button className="discover-button my-5" onClick={fetchPlaylist}>
-        Discover your Playlist
-      </button>
-      <EmbeddedSpotify spotidyId={spotifyTrackId}/>
-    </div>
-  </>
         <SaveFavouriteButton />
         <EmbeddedSpotify spotidyId={spotifyTrackId}/>
       </div>
