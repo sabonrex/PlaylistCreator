a6cb819b8ead6052eda7ef1cdd84dd4b581ad8de
romanconstantin1 <romanconstantin1@gmail.com>
2023-04-06 19:55:01 +0000

    fix: responsive component layout

--- src/front/js/component/favouriteSelect.jsx
+++ src/front/js/component/favouriteSelect.jsx
@@ -1,6 +1,9 @@
-import React, { Component, useContext } from "react";
+import React, { Component, useContext, useState } from "react";
 import { Accordion, AccordionButton, Button, Card, Col, Container, Dropdown, ListGroup, ListGroupItem, Row, Stack } from "react-bootstrap";
 import DropdownToggle from "react-bootstrap/esm/DropdownToggle";
+import Form from 'react-bootstrap/Form';
+import OverlayTrigger from 'react-bootstrap/OverlayTrigger';
+import Popover from 'react-bootstrap/Popover';
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faPlay, faEllipsisVertical, faListUl } from "@fortawesome/free-solid-svg-icons";
 
@@ -10,73 +13,85 @@ import { msToMin } from "./utils/msToMin";
 
 export const Favourites = () => {
     const {store, actions} = useContext(Context);
+    const [dropdownState, setDropdownState] = useState(false)
 
-    return (
-        <Container>
-            <Row>
-            <Col md={{ span: 8, offset: 2 }}>
-                <Card className="mb-5">
+    const favouritesComponentBuilder = (
+        <Card className="mb-5">
 
                 {Object.values(store.favouritesStore).map(entry =>
                     <>
-                    <Card.Header as="h5">Favourites - <em>{entry.length} songs</em></Card.Header>
+                    <Card.Header as="h4">Favourites - <em>{entry.length} songs</em></Card.Header>
                     <Card.Body>
+
                         <ListGroup variant="flush">                             
                             {entry.map((trackDetails, index) => (
                             <ListGroupItem action key={trackDetails.id}>
-                            <Row>
-                                <Col xs="1" className="d-flex justify-content-start my-auto">
-                                    <Dropdown>
+                            <Row className="d-flex align-items-center responsive_font">
+                                <Col xs="1">
+                                    <Dropdown autoClose={dropdownState}>
                                         <DropdownToggle variant="outline-success">
                                             <FontAwesomeIcon icon={faEllipsisVertical} />
                                         </DropdownToggle>
 
                                         <Dropdown.Menu>
                                             <Dropdown.Item onClick={() => 
-                                                (actions.removeFromFavourites("favouritesStore", trackDetails.name, index))}>
-                                                    Remove from favourites
-                                                </Dropdown.Item>
-                                            <Dropdown.Item>
-                                                Add to new playlist
+                                                (actions.removeFromFavourites("favouritesStore", trackDetails.name, index))
+                                                }>Remove from favourites
+                                            </Dropdown.Item>
+                                    
+                                            <Dropdown.Item onClick={() =>
+                                                (actions.createNewPlaylist("playlistStore", trackDetails, "New Playlist"))
+                                                }>Add to new playlist:
                                             </Dropdown.Item>
 
                                             {store.playlistStore.map(entry => 
                                                 <Dropdown.Item onClick={() => 
-                                                    (actions.addToPlaylist("playlistStore", trackDetails, entry.playlistName))}>
-                                                        Add to "{entry.playlistName}"
+                                                    (actions.addToPlaylist("playlistStore", trackDetails, entry.playlistName))
+                                                    }>Add to "{entry.playlistName}"
                                                 </Dropdown.Item>
                                             )}                                   
                                         </Dropdown.Menu>
                                     </Dropdown>
-                                    </Col>  
-                                    <Col xs="1" className="my-auto">
-                                        {index + 1}
-                                    </Col>
-                                    <Col xs="1" className="justify-content-center pe-auto">
-                                        <img src={trackDetails.album.images[2].url}></img>
-                                    </Col>
-                                    <Col xs="5" className="my-auto">
-                                        <div className="text-start ps-2">
-                                            <div className="fw-bold">{trackDetails.name}</div>
-                                            {trackDetails.artists[0].name}
-                                        </div>
-                                    </Col>
-                                    <Col xs="3" className="pe-4 my-auto text-start">
-                                        <em>{trackDetails.album.name}</em>
-                                    </Col>
-                                    <Col xs="1" className="d-flex justify-content-center my-auto">
-                                        {msToMin(trackDetails.duration_ms)}
-                                    </Col>   
-                                </Row>
-                                </ListGroupItem>))}
+                                </Col>
+
+                                <Col xs="1" className="d-flex justify-content-center">
+                                    {index + 1}
+                                </Col>
+
+                                <Col xs="2">
+                                    <img src={trackDetails.album.images[2].url}></img>
+                                </Col>
+
+                                <Col xs="4">
+                                    <div className="text-start ps-2">
+                                        <div className="fw-bold">{trackDetails.name}</div>
+                                        {trackDetails.artists[0].name}
+                                    </div>
+                                </Col>
+
+                                <Col xs="3" className="text-end">
+                                    <em>{trackDetails.album.name}</em>
+                                </Col>
+
+                                <Col xs="1" className="d-flex justify-content-center">
+                                    {msToMin(trackDetails.duration_ms)}
+                                </Col>   
+                            </Row>
+                            </ListGroupItem>))}
                                     
                         </ListGroup>                
                     </Card.Body>
                     </> )}
                 </Card>
-            </Col>
+    )
+
+    return (
+        <Container>
+            <Row>
+                <Col md={{ span: 10, offset: 1 }}>
+                    {favouritesComponentBuilder}
+                </Col>
             </Row>
         </Container>
-
     );
 }
--- src/front/js/component/playlistSelect.jsx
+++ src/front/js/component/playlistSelect.jsx
@@ -1,4 +1,4 @@
-import React, { Component, useContext } from "react";
+import React, { Component, useContext, useState } from "react";
 import {
   Accordion,
   Button,
@@ -11,6 +11,8 @@ import {
   Stack,
 } from "react-bootstrap";
 import DropdownToggle from "react-bootstrap/esm/DropdownToggle";
+import OverlayTrigger from 'react-bootstrap/OverlayTrigger';
+import Popover from 'react-bootstrap/Popover';
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faPlay, faEllipsisVertical, faListUl } from "@fortawesome/free-solid-svg-icons";
 
@@ -19,7 +21,17 @@ import { Context } from "../store/appContext";
 import { msToMin } from "./utils/msToMin";
 
 export const Playlists = () => {
-    const {store, actions} = useContext(Context)
+    const {store, actions} = useContext(Context);
+    const [dropdownState, setDropdownState] = useState(false);
+
+    const popover = (
+        <Popover id="popover-basic" onClick={() => setDropdownState(true)}>
+            <Popover.Body >
+                And here's some <strong>amazing</strong> content. It's very engaging.
+                right?
+            </Popover.Body>
+        </Popover>
+    );
 
     const playlistComponentBuilder = (
         <Accordion>
@@ -35,59 +47,67 @@ export const Playlists = () => {
                         <ListGroup variant="flush">
                             {entry.list.tracks.map((trackDetails, index) =>
                                 <ListGroupItem action key={trackDetails.id}>
-                                <Row>
-                                <Col xs="1" className="d-flex justify-content-start my-auto">
-                                    <Dropdown>
-                                        <DropdownToggle variant="outline-success">
+                                <Row Row className="d-flex align-items-center responsive_font">
+                                <Col xs="1">
+                                    <Dropdown autoClose={dropdownState}>
+
+                                        <DropdownToggle variant="outline-success" >
                                             <FontAwesomeIcon icon={faEllipsisVertical} />
                                         </DropdownToggle>
-
                                         <Dropdown.Menu>
-                                            <Dropdown.Item onClick={() => 
-                                                (actions.addToFavourites("favouritesStore", trackDetails))}
-                                                >Add to Favourites
+
+                                            <Dropdown.Item onClick={() => (
+                                                actions.addToFavourites("favouritesStore", trackDetails)
+                                                )}> Add to Favourites
                                             </Dropdown.Item>
+
                                             <Dropdown.Item onClick={() => (
                                                 actions.removeFromPlaylist("playlistStore", index, entry.playlistName)
-                                                )}>
-                                                Remove from this playlist
+                                                )}> Remove from this playlist                                               
                                             </Dropdown.Item>
+
+                                            <OverlayTrigger trigger="click" placement="right" overlay={popover}>
+                                                <Dropdown.Item> Move to new playlist:
+                                                </Dropdown.Item>
+                                            </OverlayTrigger>
+                                            
 
                                             {store.playlistStore.map(storeEntry => 
                                                 {if (storeEntry.playlistName !== entry.playlistName) {
                                                     return (
                                                     <Dropdown.Item onClick={() => 
-                                                    actions.moveToPlaylist("playlistStore", trackDetails, index, entry.playlistName, storeEntry.playlistName)}>
-                                                        Move to "{storeEntry.playlistName}"
+                                                    actions.moveToPlaylist(
+                                                        "playlistStore", trackDetails, index, entry.playlistName, storeEntry.playlistName
+                                                        )}> Move to "{storeEntry.playlistName}"                                                       
                                                     </Dropdown.Item>
                                                     )}                                                
                                                 }
-                                            )} 
+                                            )};
                                         </Dropdown.Menu>
 
                                     </Dropdown>
-                                    </Col>   
+                                </Col>   
 
-                                    <Col xs="1" className="my-auto">
+                                    <Col xs="1" className="d-flex justify-content-center">
                                         {index + 1}
                                     </Col>
 
-                                    <Col xs="1" className="justify-content-center me-auto">
+                                    <Col xs="2 pe-1">
                                         <img src={trackDetails.album.images[2].url}></img>
                                     </Col>
 
-                                    <Col xs="5" className="my-auto">
-                                        <div className="text-start ps-2">
-                                            <div className="fw-bold">{trackDetails.name}</div>
+                                    <Col xs="4">
+                                        <div className="text-start">
+                                            <div className="fw-bold" >{trackDetails.name}</div>
                                             {trackDetails.artists[0].name}
                                         </div>
                                     </Col>
 
-                                    <Col xs="3" className="pe-4 my-auto text-end">
+                                    <Col xs="3" className="text-end">
                                         <em>{trackDetails.album.name}</em>
                                     </Col>
 
-                                    <Col xs="1" className="d-flex justify-content-center my-auto">
+                                    <Col xs="1" className="d-flex justify-content-center">
                                         {msToMin(trackDetails.duration_ms)}
                                     </Col>   
 
@@ -97,16 +117,16 @@ export const Playlists = () => {
                               </ListGroup>
                     </Accordion.Body>
                 </Accordion.Item>
-            )}
+            )};
         </Accordion>
     )
 
     return (
         <Container className = "pb-3">
             <Row>
-            <Col md={{ span: 8, offset: 2 }}>
-                {playlistComponentBuilder}
-            </Col>
+                <Col md={{ span: 10, offset: 1 }}>
+                    {playlistComponentBuilder}
+                </Col>
             </Row>
         </Container>
     );
