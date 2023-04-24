6ce4867c85ef24b11d0d40899ce490fa07e30c72
romanconstantin1 <romanconstantin1@gmail.com>
2023-04-24 09:44:18 +0000

    chore: removed leftover garbage code after last merge

--- src/front/js/component/embeddedSpotifyTrack.jsx
+++ src/front/js/component/embeddedSpotifyTrack.jsx
@@ -46,72 +46,8 @@ const EmbeddedSpotify = ({ trackId }) => {
     <div>
       <div ref={iframeRef}></div>
       {console.log(trackId)}
-      {/* <iframe 
-              className="spotify_embed"
-              style={{ borderRadius:"12px" }}
-              src={`https://open.spotify.com/embed/track/${trackId}?utm_source=generator`}
-              width="100%" height="85" 
-              allowFullScreen="" 
-              allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture" 
-              loading="lazy"
-            ></iframe> */}
     </div>
   );
 }
 
 export default EmbeddedSpotify;
-
-
-
-// const spotifyApi = new SpotifyWebApi();
-
-// function EmbeddedSpotify() {
-//   const {store, actions} = useContext(Context)
-//   const playerRef = useRef(null);
-//   spotifyApi.setAccessToken(actions.getAccessToken)
-  
-//   useEffect(() => {
-//     const player = playerRef.current;
-    
-//     player.src = `https://open.spotify.com/embed/track/${store.nowPlaying}`;
-//     player.allow = 'autoplay';
-
-//     // Play the player automatically
-//     player.contentWindow.postMessage(JSON.stringify({ method: 'play' }), 'https://open.spotify.com');
-
-//     const onLoad = () => {
-//       player.contentWindow.postMessage(JSON.stringify({ method: 'play' }), 'https://open.spotify.com');
-//     };
-
-//     player.addEventListener('load', onLoad);
-
-//     return () => {
-//       // Pause the player when the component unmounts
-//       player.contentWindow.postMessage(JSON.stringify({ method: 'pause' }), 'https://open.spotify.com');
-//       player.removeEventListener('load', onLoad);
-//     };
-//   }, [store.nowPlaying]);
-
-//   return (
-//     <iframe
-//       ref={playerRef}
-//       width="100%"
-//       height="300"
-//       frameBorder="0"
-//       allowtransparency="true"
-//       allow="encrypted-media"
-//     />
-//   );
-// }
-
-// export default EmbeddedSpotify;
-
-{/* <iframe 
-              className="spotify_embed"
-              style={{ borderRadius:"12px" }}
-              src={`https://open.spotify.com/embed/track/${store.nowPlaying}?utm_source=generator`}
-              width="100%" height="85" 
-              allowFullScreen="" 
-              allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture" 
-              loading="lazy"
-            ></iframe> */}
