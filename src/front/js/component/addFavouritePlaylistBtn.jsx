fec714c0716b2500c0d948bc831e506910b22cb9
José Inácio Castelo Branco <zeinaciocb@gmail.com>
2023-04-26 23:31:27 +0000

    fix(front): styles cleaned, logic to be added

--- src/front/js/component/addFavouritePlaylistBtn.jsx
+++ src/front/js/component/addFavouritePlaylistBtn.jsx
@@ -1,7 +1,7 @@
 import React, { useContext } from "react";
 
-import { Button } from "react-bootstrap";
 import { Context } from "../store/appContext";
+import { getToken } from "../auth/getToken.js";
 
 export const AddFavouritePlaylistButton = () => {
     const { store } = useContext(Context);
@@ -14,7 +14,31 @@ export const AddFavouritePlaylistButton = () => {
     }
 
     return (
-      <button className="fav-playlist-button my-3" onClick={handleClick} >
+      <button className="fav-playlist-button mb-5" onClick={handleClick} >
         Do you like it? Save it!
       </button>
-    )}
+    )
+};
+
+const addUserFavouritePlaylist = async (playlistId) => {
+  const { store } = useContext(Context);
+  const token = getToken();
+  const endpoint = `${store.apiUrl}/api/user/favourites/playlists/${playlistId}`
+
+  try {
+    const response = await fetch(endpoint, {
+      method: "POST",
+      headers: {
+          "Authorization": `Bearer ${token}`,
+          "Content-Type": "application/json"
+      }
+    });
+
+    const jsonResponse = await response.json()
+    return jsonResponse
+
+  } catch (error) {
+    console.error(error)
+  }
+
+}
